3c1660877384bb8d50d43b384f11751fdd8dfa40
yuriy_beloff <paganismrus@gmail.com>
2017-11-07 16:48:13 +0300

    complete

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+This module checks strength of password from 1 to 10
+
+# Usage Examples
+
+    $ python password_strength.py asdasd
+    1
+
+    $ python password_strength.py admin
+    Your password is in password blacklist
+
+    $ python password_strength.py asd12SA
+    10
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,45 @@
+import sys
+
+
+def get_password_blacklist():
+    with open('pass.txt', 'r') as file:
+        black_list = file.read().split()
+        return black_list
 
 
 def get_password_strength(password):
-    pass
+    lower_case = 0
+    upper_case = 0
+    digit_case = 0
+    spec_char = 0
+    rating = 0
+    for letter in password:
+        if letter.isupper():
+            upper_case += 1
+        elif letter.isdigit():
+            digit_case += 1
+        elif not letter.isalpha() and not letter.isdigit():
+            spec_char += 1
+        else:
+            lower_case += 1
+    if upper_case > 0 and lower_case > 0:
+        rating += 2
+        # print("upper and lower", rating)
+    if digit_case > 0:
+        rating += 2
+        # print("digit", rating)
+    if spec_char > 0:
+        rating += 4
+        # print("spec", rating)
+    password_blacklist = get_password_blacklist()
+    if password_blacklist.count(password) > 0:
+        # print("your password is in blacklist")
+        rating = 1
+    else:
+        rating += 2
+    return rating
 
 
 if __name__ == '__main__':
-    pass
+    password = sys.argv[1]
+    print(get_password_strength(password))
